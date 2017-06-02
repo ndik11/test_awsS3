9f0a2e516fa843f00622d4279148464575634575
ndik11 <ndik11@gmail.com>
2017-06-02 12:08:14 +0300

    pentest and limitchecker added

--- aws.py
+++ aws.py
@@ -8,10 +8,27 @@ import time
 import whois
 import requests
 import os
+from netaddr import *
+import pprint
+from ipwhois import *
+
+# AAAS-4574-deploy-services-needs-to-have-a
+
+def test():
+    for i in range(0, 30):
+        while True:
+            try:
+                print(i/0)
+            except:
+                print("lalala", i)
+            break
+        if i==29:
+            print("Too many requests. Limit is 30 times")
 
 
-# for key in sorter:
-#     print(key, bigrams[key])
+
+
+
 
 
 def sg_info(sg):
@@ -31,9 +48,20 @@ def sg_info(sg):
             except:
                 print('There are no Inbound rules')
             for d in c['IpRanges']:
-                try:
+                if d['CidrIp'] != '0.0.0.0/0':
+                    # d['CidrIp'] = '216.70.7.11/32'
                     print('Inbound IP Ranges:', d['CidrIp'])
-                except:
+                    print('All the IPs:')
+                    xz = d['CidrIp']
+                    ips = IPNetwork(xz)
+                    print('!!!!!!!!!!!!!!!!!!!')
+                    print(ips.ip)
+                    obj = IPWhois(ips.ip)
+                    results = obj.lookup_rdap(depth=1)
+                    pp = pprint.PrettyPrinter(indent=2)
+                    print('Whois results: ', pp.pprint(results))
+                    print('OLOLOLOLO')
+                else:
                     print('There are no Inbound IP Ranges')
         for c in i['IpPermissionsEgress']:
             print('IpPermissionsEgress.', 'Protocol: ', c['IpProtocol'])
@@ -62,7 +90,7 @@ def network_interfaces(instance):
         for k in r['Instances']:
             if k['State']['Name'] == 'running':
                 for i in k['NetworkInterfaces']:
-                    ni_responce = 'Instance is Running. Public IP:', i['Association']['PublicIp']
+                    ni_responce = ''.join(['Instance is Running. Public IP:', i['Association']['PublicIp']])
             else:
                 ni_responce = 'Instance is not running'
     return ni_responce
@@ -72,8 +100,8 @@ def network_interfaces(instance):
 
 
 def run_scan():
-    s3 = boto3.resource('s3')
-    client = boto3.client('ec2')
+    # s3 = boto3.resource('s3')
+    client = boto3.client('ec2', region_name='us-west-2')
     response = client.describe_instances(
         DryRun=False,
         InstanceIds=[],
@@ -81,31 +109,22 @@ def run_scan():
     )
     case_list1 = []
     case_list2 = []
-    f_json = open('wow.json', 'w', encoding='utf-8')
-    f = open('file.txt', 'w', encoding='utf-8')
-    # inst_id_dict = {}.fromkeys(['InstanceId', 'InstanceType'])
-    print('AWS AccountId 771749627727', '\n', file=f)
-    print('SubmitterName Howard Wang', '\n', file=f)
-    print('CompanyName Entrust Datacard Corp.', '\n', file=f)
-    print('EmailAddress howard.wang@entrustdatacard.com', '\n', file=f)
-    print('AdditionalEmail1 Duncan.Stirling@entrust.com', '\n', file=f)
-    print('AdditionalEmail2# tim.clarke@entrust.com', '\n', file=f)
-    # print(response)
+    for_email = []
+    info1 = []
     for r in response['Reservations']:
         for i in r['Instances']:
-            print('Instance name: ', i['InstanceId'], 'Type:', i['InstanceType'], 'Image ID:', i['ImageId'], '\n', file=f)
-            print(network_interfaces(i['InstanceId']), '\n', file=f)
-            print('==============', file=f)
+            info1 = ['\n', 'Instance name: ', i['InstanceId'], 'Type:', i['InstanceType'], 'Image ID:', i['ImageId'], '\n', network_interfaces(i['InstanceId']), '\n', '==============']
+            for_email.append(info1)
             case1 = i['InstanceId']
             case_list1.append(case1)
             case2 = i['InstanceType']
             case_list2.append(case2)
             # inst_id_dict = {'InstanceId': i['InstanceId'], 'InstanceType': i['InstanceType']}
             # inst_id_dict.update({'InstanceId': i['InstanceId'], 'InstanceType': i['InstanceType']})
-            # for b in i['SecurityGroups']:
-            #     print('Security group name: ', b['GroupName'], 'Group ID: ', b['GroupId'])
-            #     sg_info(b['GroupId'])
-            #     print('==============')
+            for b in i['SecurityGroups']:
+                print('Security group name: ', b['GroupName'], 'Group ID: ', b['GroupId'])
+                sg_info(b['GroupId'])
+                print('==============')
             # dict1 = zip(case_list1, case_list2)
             # for case_list1, case_list2 in dict1:
             #     print(case_list1, case_list2)
@@ -113,20 +132,36 @@ def run_scan():
             # json1 = json.dump({"InstanceId": i['InstanceId'], "InstanceType": i['InstanceType'], }, sort_keys=True,
             #                   indent=4, separators=(',', ': '), fp=f_json)
 
-    # for i in case_list1:
-    #     case3 = json.dump({"InstanceId": i},
-    #                       indent=4, separators=(',', ': '), fp=f_json)
-    #     for k in case_list2:
-    #         case3 = json.dump({"InstanceType": k},
-    #                        indent=4, separators=(',', ': '), fp=f_json)
-    #         break
-    case_list1 = json.dump({"InstanceId": case_list1, "InstanceType": case_list2}, sort_keys=True,
-                           indent=4, separators=(',', ': '), fp=f_json)
-    f.close()
-    f_json.close()
-    # print(case_list1)
-    # print(case_list1, case_list2)
-    return case_list1, case_list2
+    for_db = json.dumps({"InstanceId": case_list1, "InstanceType": case_list2}, sort_keys=False,
+                           indent=4, separators=(',', ': '))
+    message = ''
+    client = boto3.client("ses")
+    for i in for_email:
+        # ''.join(list(i[7]))
+        ii = ''.join(list(i))
+        message += ii
+    # message = repr(for_email)
+    print(message)
+    info = 'Email Address			aws-acs-prod@entrustdatacard.com,\nAWS AccountId: 771749627727,\nSubmitterName: ' \
+           'Howard Wang,\nCompanyName: EntrustDatacard Corp,' \
+           '\nEmailAddress: howard.wang@entrustdatacard.com,\n AdditionalEmail1: Duncan.Stirling@entrust.com, ' \
+           '\n AdditionalEmail2#: tim.clarke@entrust.com \n'
+    rowMess = 'From: AndreyMakarov@coherentsolutions.com\nTo: AndreyMakarov@coherentsolutions.com\nSubject: Your AWS Penetration Testing Inquiry\nMIME-Version: 1.0\n\n{0}\n{1}\n\nBandwidth\t\t\t1\nRegion\t\t    \tVirginia\nTimezone\t\t\tgmt--3\nStartDate\t\t\t\nEndDate\t\t\t\nComments\t\t\t\nTermsAgreement\t\t\ti-agree\nPolicy\t    \t\ti-agree\nAWS Account ID\t\t\t771749627727\nAWS Account ID\t\t\t771749627727\nIAM Account\t\t\tNo\nEmail Address\t\t\taws-acs-prod@entrustdatacard.com\nName\t\t\tacs_production '.format(
+        info, message)
+
+    response = client.send_raw_email(
+        Destinations=[
+        ],
+        FromArn='',
+        RawMessage={
+            'Data': rowMess,
+        },
+        SourceArn='arn:aws:ses:us-west-2:242906888793:identity/AndreyMakarov@coherentsolutions.com',
+    )
+
+    print(response)
+
+    return for_db
 
 
 # run_scan()
@@ -165,64 +200,6 @@ def create_table():
 # create_table()
 
 
-def load_data():
-    import json
-    import decimal
-    dynamodb = boto3.resource('dynamodb')
-    import datetime
-    my_time = str(datetime.datetime.now())
-    print(my_time)
-    table = dynamodb.Table('Instances')
-
-    with open("wow.json") as json_file:
-        opened_json = json.load(json_file, parse_float=decimal.Decimal)
-        for i in opened_json['InstanceId']:
-            InstanceId = i
-            InstanceId = str(InstanceId)
-            for k in opened_json['InstanceType']:
-                InstanceType = k
-                InstanceType = str(InstanceType)
-            print("Adding InstanceId:", InstanceId, InstanceType)
-
-
-            status = table.put_item(
-                Item={
-                    'TimeStamp': my_time,
-                    'InstanceId': InstanceId,
-                    'InstanceType': InstanceType,
-                }
-            )
-            print(status)
-load_data()
-
-
-
-def send_11email():
-    client = boto3.client("ses")
-    f = open('file.txt', 'r', encoding='utf-8')
-    message = f.read()
-    rowMess = 'From: @.com\nTo: @\nSubject: Test ' \
-              'email (contains an attachment)\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; ' \
-              'boundary="NextPart"\n\n--NextPart\nContent-Type: text/plain\n\n' + message \
-              + '\n\n--NextPart\nContent-Type: text/plain;\nContent-Disposition: attachment; ' \
-                'filename="attachment.txt"\n\nThis is the text in the attachment.\n\n--NextPart-- '
-
-    response = client.send_raw_email(
-        Destinations=[
-        ],
-        FromArn='',
-        RawMessage={
-            'Data': rowMess,
-        },
-        SourceArn='arn:aws:ses:us-west-2:2429:identity/@.com',
-    )
-
-    print(response)
-
-
-# send_11email()
-
-
 def create_bucket(new_bucket):
     s3 = boto3.resource('s3')
     for bucket in s3.buckets.all():
@@ -256,6 +233,7 @@ def upload_file(file_name):
         print('Not sent')
     for key, value in dict_responce.get('HTTPHeaders').items():
         print(key, value)
+
 
 # create_bucket(new_bucket)
 # upload_file(file_name)
---
+++ limitchecker.py
@@ -0,0 +1,115 @@
+from __future__ import print_function
+from awslimitchecker.checker import AwsLimitChecker
+import boto3
+import os
+
+''' This function is to run the limitchecker api and alert on any limits which are seen to be close to their limits.
+- The limitchecker has several drawbacks. We can't check all the limits due Amazon don't have API to check almost 60% of them. So now we check just the limits that can be validated through API or Trusted Advisor API.
+- The variables below are set when the lambda function is run.
+'''
+print('Loading function')
+#email to send from
+email_from = os.environ['email_from']
+#email send to
+email_to= os.environ['email_to']
+#arn to send from
+email_from_arn = os.environ['email_from_arn']
+#list of emails
+email_cc_for_case = os.environ['email_cc_for_case']
+
+def lambda_handler(event, context):
+    limit_checker()
+
+
+def limit_checker():
+    c = AwsLimitChecker()
+    result = c.check_thresholds()
+    warning_message = ''
+    critical_message = ''
+
+    for service, svc_limits in result.items():
+        for limit_name, limit in svc_limits.items():
+            if (limit.api_limit is not None) or (limit.ta_limit is not None):
+                for warn in limit.get_warnings():
+                    warning_notice = ("\n{service} '{limit_name}' usage ({u}) exceeds "
+                                      "warning threshold (limit={l})".format(
+                            service=service,
+                            limit_name=limit_name,
+                            u=str(warn),
+                            l=limit.get_limit(),
+                        )
+                    )
+                    if limit.get_limit() < 100:
+                        warning_message += ''.join(list(warning_notice)) + " . Please raise it up to five times more\n"
+                    elif limit.get_limit() < 1000:
+                        warning_message += ''.join(list(warning_notice)) + ". Please raise it up to 50%\n"
+                    elif limit.get_limit() > 1000:
+                        warning_message += ''.join(list(warning_notice)) + ". Please raise it up to 20%\n"
+
+                for crit in limit.get_criticals():
+                    if not crit:
+                        have_critical = False
+                        print(have_critical)
+                    critical_notice = ("{service} '{limit_name}' usage ({u}) exceeds "
+                                       "critical threshold (limit={l})".format(
+                            service=service,
+                            limit_name=limit_name,
+                            u=str(crit),
+                            l=limit.get_limit(),
+                        )
+                    )
+                    if limit.get_limit() < 100:
+                        critical_message += ''.join(list(critical_notice)) + ". Please raise it up to five times more\n"
+                    elif limit.get_limit() < 1000:
+                        critical_message += ''.join(list(critical_notice)) + ". Please raise it up to 50%\n"
+                    elif limit.get_limit() > 1000:
+                        critical_message += ''.join(list(critical_notice)) + ". Please raise it up to 20%\n"
+
+
+    print("WARNING MESSAGE:", warning_message, "CRITICAL MESSAGE:\n", critical_message)
+    if len(critical_message) > 10:
+        print('Critical found')
+        case_id = case_creation(warning_message, critical_message)
+        email_send(case_id, warning_message, critical_message)
+    else:
+        print("No limit criticals")
+
+
+def case_creation(warning_message, critical_message):
+    client_support = boto3.client('support')
+    response = client_support.create_case(
+        subject='Limits',
+        serviceCode='aws-identity-and-access-management',
+        severityCode='low',
+        categoryCode='other',
+        communicationBody='Hello\n' + warning_message + critical_message,
+        ccEmailAddresses=[
+            ''+ email_cc_for_case
+        ],
+        language='en',
+        issueType='customer-service',
+    )
+    case_id = response['caseId']
+    print(case_id, response['caseId'])
+    return case_id
+
+
+# 'Will.Wightman@entrustdatacard.com', 'EESDevOps@entrustdatacard.com', 'marksomm@amazon.com'
+
+# email!!!!!!!!!!!!!!!!!!!!!!!!!!
+def email_send(case_id, warning_message, critical_message):
+    client = boto3.client("ses")
+    info = 'We have some warnings:  '
+    case_created = '\nCase#' + case_id
+    rowMess = 'From: {0}\nTo: {1}'.format(email_from, email_to) + '\nSubject: AWS limits checker' \
+              'check\nMIME-Version: 1.0\n\n' + info + warning_message + critical_message + case_created
+    response = client.send_raw_email(
+        Destinations=[
+        ],
+        FromArn='',
+        RawMessage={
+            'Data': rowMess,
+        },
+        SourceArn='' + email_from_arn,
+    )
+    print('Email info:', response)
